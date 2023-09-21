e1f92a7f9239fe9146c9d9164607df3aff0c16f5
Loic-de-tlse3 <loic.francois1@univ-tlse3.fr>
2023-09-21 15:30:55 +0200

    ajout de méthode sur la classe Gaulois et crétion de la classe Romain

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,11 +4,33 @@ public class Gaulois {
 	private String nom;
 	private int force;
 	private int effetPotion = 1;
-
-	public Gaulois(String nom, int force, int effetPotion) {
-		super();
+	
+	public Gaulois(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
-		this.effetPotion = effetPotion;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public int getForce() {
+		return force;
+	}
+
+	public int getEffetPotion() {
+		return effetPotion;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole(getNom()) +  texte);
+	}
+
+	private String prendreParole(String nom) {
+		return "Le gaulois " + nom + " : ";
+	}
+	
+	public String toString() {
+		return "Gaulois [nom=" + getNom() + ", force=" + getForce() + ", effetPotion=" + getEffetPotion() + "]";
 	}
 }
