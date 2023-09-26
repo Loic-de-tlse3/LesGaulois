312c597ee03a148f09976ded3f0ed0407e111bca
Loic-de-tlse3 <loic.francois1@univ-tlse3.fr>
2023-09-26 19:08:11 +0200

    TP1 commit2

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,16 +4,16 @@ public class Gaulois {
 	private String nom;
 	private int force;
 	private int effetPotion = 1;
-	
+
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 	}
-	
+
 	public String getNom() {
 		return nom;
 	}
-	
+
 	public int getForce() {
 		return force;
 	}
@@ -23,24 +23,25 @@ public class Gaulois {
 	}
 
 	public void parler(String texte) {
-		System.out.println(prendreParole(getNom()) +  texte);
+		System.out.println(prendreParole(getNom()) + texte);
 	}
 
 	private String prendreParole(String nom) {
 		return "Le gaulois " + nom + " : ";
 	}
-	
+
 	public void frapper(Romain romain) {
 		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-		romain.recevoirCoup(force/3);
+		romain.recevoirCoup(force / 3);
 	}
-	
+
 	public String toString() {
 		return "Gaulois [nom=" + getNom() + ", force=" + getForce() + ", effetPotion=" + getEffetPotion() + "]";
+		System.out.println(asterix);
 	}
-	
+
 	public static void main(String[] args) {
-		
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		toString();
 	}
 }
-
