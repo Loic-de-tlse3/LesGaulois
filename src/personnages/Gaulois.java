3b4ccb2cfc33664585697a92ed03a955b1da41bb
loic-de-tlse3 <loic.francois1@univ-tlse3>
2023-10-03 17:45:02 +0200

    Ajout modification TP3

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,6 +4,8 @@ public class Gaulois {
     private String nom;
     private int force;
     private int effetPotion = 1;
+    private int force, nb_trophees;
+    private Equipement trophees[] = new Equipement[100];
 
     public Gaulois(String nom, int force) {
         this.nom = nom;
@@ -26,13 +28,27 @@ public class Gaulois {
         System.out.println(prendreParole(getNom()) + texte);
     }
 
-    private String prendreParole(String nom) {
-        return "Le gaulois " + nom + " : ";
+//    private String prendreParole(String nom) {
+//        return "Le gaulois " + nom + " : ";
+//    }
+    
+    private String prendreParole() {
+    	String texte = "Le gaulois " + nom + " : ";
+    	return texte;
     }
 
+//    public void frapper(Romain romain) {
+//        System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+//        romain.recevoirCoup(force / 3 * effetPotion);
+//    }
+    
     public void frapper(Romain romain) {
-        System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-        romain.recevoirCoup(force / 3 * effetPotion);
+    	System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+    	Equipement trophees[] = romain.recevoirCoup((force / 3) * effetPotion);
+    	for (int i = 0; trophees != null && i < trophees.length; i++, nb_trophees++) {
+    		this.trophees[nb_trophees] = trophees[i];
+    	}
+    	return;
     }
 
     public String toString() {
@@ -43,6 +59,7 @@ public class Gaulois {
         effetPotion = druide.getForcePotion();
         parler("« Merci Druide, je sens que ma force est " + effetPotion + " fois décuplée. »");
     }
+    
 
     public static void main(String[] args) {
         Gaulois asterix = new Gaulois("Astérix", 8);
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -5,9 +5,10 @@ public class Romain {
 	private int force;
 	private Equipement[] equipements = new Equipement[2];
 	private int nbEquipement = 0;
-	
+	private String texte;
+
 	public Romain(String nom, int force) {
-		assert(force>0);
+		assert (force > 0);
 		this.nom = nom;
 		this.force = force;
 	}
@@ -19,51 +20,114 @@ public class Romain {
 	public int getForce() {
 		return force;
 	}
-	
+
 	public void parler(String texte) {
-		System.out.println(prendreParole(getNom()) +  texte);
+		System.out.println(prendreParole(getNom()) + texte);
 	}
 
 	private String prendreParole(String nom) {
 		return "Le romain " + nom + " : ";
 	}
+
+//	public void recevoirCoup(int forceCoup) {
+//		assert (this.force > 0);
+//		int forceAvantCoup = this.force;
+//		force -= forceCoup;
+//		if (force > 0) {
+//			parler("Aie");
+//		} else {
+//			parler("J'abandonne...");
+//		}
+//		assert (forceAvantCoup > this.force);
+//	}
 	
-	public void recevoirCoup(int forceCoup) {
-		assert(this.force>0);
-		int forceAvantCoup = this.force;
+	public Equipement[] recevoirCoup(int forceCoup) {
+		Equipement[] equipementEjecte = null;
+		// précondition
+		assert force > 0;
+		int oldForce = force;
+		forceCoup = CalculResistanceEquipement(forceCoup);
 		force -= forceCoup;
-		if (force > 0) {
-			parler("Aie");
-		} else {
+// 		if (force > 0) {
+// 			parler("Aïe");
+// 		} else {
+// 			equipementEjecte = ejecterEquipement();
+// 			parler("J'abandonne...");
+// 		}
+		switch (force) {
+		case 0:
+			parler("Aïe");
+		default:
+			equipementEjecte = ejecterEquipement();
 			parler("J'abandonne...");
+			break;
 		}
-		assert(forceAvantCoup > this.force);
+// 		post condition la force a diminuée
+		assert force < oldForce;
+		return equipementEjecte;
+	}
+	
+	private int CalculResistanceEquipement(int forceCoup) {
+		texte = "Ma force est de " + this.force + ", et la force du coup est de " + forceCoup;
+		int resistanceEquipement = 0;
+		if (!(nbEquipement == 0)) {
+			texte += "\nMais heureusement, grace à mon équipement sa force est diminué de ";
+			for (int i = 0; i < nbEquipement;) {
+					if ((equipements[i] != null && equipements[i].equals(Equipement.BOUCLIER)) == true) {
+						resistanceEquipement += 8;
+					} else {
+						System.out.println("Equipement casque");
+						resistanceEquipement += 5;
+					}
+					i++;
+			}
+			texte =+ resistanceEquipement + "!";
+		}
+		parler(texte);
+		forceCoup -= resistanceEquipement;
+		return forceCoup;
+	}
+	
+	private Equipement[] ejecterEquipement() {
+		Equipement[] equipementEjecte = new Equipement[nbEquipement];
+		System.out.println("L'équipement de " + nom.toString() + "s'envole sous la force du coup.");
+		//TODO
+		int nbEquipementEjecte = 0;
+		for (int i = 0; i < nbEquipement; i++) {
+			if (equipements[i] == null) {
+				continue;
+			} else {
+				equipementEjecte[nbEquipementEjecte] = equipements[i];
+				nbEquipementEjecte++;
+				equipements[i] = null;
+			}
+		}
+		return equipementEjecte;
 	}
 	
 	public void sEquiper(Equipement equipement) {
-		switch(nbEquipement) {
-			case 2 :
-				System.out.println("Le soldat " + nom + " est déjà bien protégé(e) !");
-				break;
-			case 1 :
-				if (equipements[0] == equipement){
-					System.out.println("Le soldat " + nom + " possède déjà un " + equipement);
-				} 
-				else {
-					this.equiper(equipement);
-				}
-				break;
-			default :
+		switch (nbEquipement) {
+		case 2:
+			System.out.println("Le soldat " + nom + " est déjà bien protégé(e) !");
+			break;
+		case 1:
+			if (equipements[0] == equipement) {
+				System.out.println("Le soldat " + nom + " possède déjà un " + equipement);
+			} else {
 				this.equiper(equipement);
+			}
+			break;
+		default:
+			this.equiper(equipement);
 		}
 	}
-	
+
 	private void equiper(Equipement equipement) {
 		equipements[nbEquipement] = equipement;
 		System.out.println("Le soldat " + nom + " s'équipe d'un " + equipement);
 		nbEquipement++;
 	}
-	
+
 	public static void main(String[] args) {
 		Romain unRomain = new Romain("UnRomain", 6);
 		System.out.println(unRomain.prendreParole(unRomain.getNom()));
